a3edea6367b816c816f38663aba5fcaf6ef7ab21
Maory12 <124795072+Maory12@users.noreply.github.com>
2023-04-26 13:37:52 +0000

    Co-authored-by: Ivan Rivalcoba <ivan.rivalcoba@gmail.com>

--- server/app.js
+++ server/app.js
@@ -79,7 +79,7 @@ app.use('/users', usersRouter);
 app.use(apiRouter);
 
 // catch 404 and forward to error handler
-app.use((err, req, res, next) => {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
