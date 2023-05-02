48f6d2f0a04fba24c3ba589f082f13f54ff003ac
Maory12 <124795072+Maory12@users.noreply.github.com>
2023-05-02 08:50:18 +0000

    Materialize Css🎨

--- client/index.js
+++ client/index.js
@@ -1,29 +1,43 @@
+// import './styles/style.css';
+
+// console.log('🎉 Estilos cargados correctamente 🎉');
+
+// // ES6 CODE
+// // Default Parameters
+// const show = (msg = 'No message given') => {
+//   console.log(msg);
+// };
+
+// show();
+
+// // ES6 CODE
+// // Async Await
+// function resolveAfter2Seconds() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve('resolved');
+//     }, 2000);
+//   });
+// }
+
+// async function asyncCall() {
+//   console.log('Calling');
+//   const result = await resolveAfter2Seconds();
+//   console.log(result);
+// }
+
+// asyncCall();
+// cargando los estilos
+/* eslint-disable no-console */
 import './styles/style.css';
+/* eslint-disable */
+// Importandoe Estilos de Materialize CSS
+import 'materialize-css/dist/css/materialize.css';
+// Importando scripts de Materialize
+import 'materialize-css/dist/js/materialize';
+/* eslint-enable */
 
-console.log('🎉 Estilos cargados correctamente 🎉');
+// Inicializando Scripts de Materialize para interactividad
+M.AutoInit();
 
-// ES6 CODE
-// Default Parameters
-const show = (msg = 'No message given') => {
-  console.log(msg);
-};
-
-show();
-
-// ES6 CODE
-// Async Await
-function resolveAfter2Seconds() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('resolved');
-    }, 2000);
-  });
-}
-
-async function asyncCall() {
-  console.log('Calling');
-  const result = await resolveAfter2Seconds();
-  console.log(result);
-}
-
-asyncCall();
+console.log('Webpack Working Middleware!!! 📦');
